2a495f935249e3d66c62ee87870074647cefa739
NastyaZhilko <nasten4ik2010@gmail.com>
2021-02-04 01:02:10 +0300

    first commit

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import s from './HW5.module.css'
+import {HashRouter} from "react-router-dom";
+
+function Header() {
+    return (
+        <div>
+
+            <HashRouter>
+            </HashRouter>
+        </div>
+    );
+}
+
+export default Header;
