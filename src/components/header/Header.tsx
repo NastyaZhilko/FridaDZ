84b333beef84643866cdb20e8cdafb24c3958726
VilenaArturovna <vilena-forever@inbox.ru>
2021-02-12 20:58:18 +0600

    auth added

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,9 +3,15 @@ import {NavLink} from "react-router-dom";
 
 import s from './Header.module.css';
 import {PATH} from "../routes/Routes";
+import {useDispatch, useSelector} from "react-redux";
+import {AppStoreType} from "../../store/store";
+import {logout} from "../../store/login-reducer";
 
 function Header() {
     const [activeMenu, setActiveMenu] = useState<boolean>(false);
+    const isAuth = useSelector<AppStoreType, boolean>(state => state.login.isAuth)
+    const dispatch = useDispatch()
+
     const toggle = () => {
         setActiveMenu(!activeMenu)
     }
@@ -21,6 +27,8 @@ function Header() {
                          activeClassName={s.active}>  Registration  </NavLink>
                 <NavLink to={PATH.newPassword} className={s.inactive}
                          activeClassName={s.active}>  Entering a new password  </NavLink>
+                {isAuth ? <button onClick={() => dispatch(logout)}>Log out</button> :
+                    <NavLink to={'/login'}>Login</NavLink>}
 
             </div>
         </div>
--- src/components/pages/login.tsx
+++ src/components/pages/login.tsx
@@ -1,24 +1,49 @@
-
-import React from "react";
+import React, { FormEvent } from "react";
 import SuperInput from "../common/SuperInput/SuperInput";
 import SuperCheckbox from "../common/SuperCheckbox/SuperCheckbox";
 import SuperButton from "../common/SuperButton/SuperButton";
+import {useDispatch, useSelector} from "react-redux";
+import {login, LoginFormData} from "../../store/login-reducer";
+import {AppStoreType} from "../../store/store";
+import {Redirect} from "react-router-dom";
 
 export function Login() {
+
+    const dispatch = useDispatch()
+    const isAuth = useSelector<AppStoreType, boolean>(state => state.login.isAuth)
+
+    if (isAuth) {
+        return <Redirect to={'/profile'} />
+    }
+
+    const submitLoginData = (e: FormEvent<HTMLFormElement>) => {
+        debugger
+        e.preventDefault();
+        const email = e.currentTarget.email.value
+        const password = e.currentTarget.password.value
+        const rememberMe: boolean = e.currentTarget.rememberMe.value
+        const data: LoginFormData = {email, password, rememberMe}
+        debugger
+        dispatch(login(data))
+    }
+
     const loginStyle = {
         'display': 'flex',
         'flex-direction': 'column',
         'align-items': 'center'
     }
+
     return (
-        <div style={loginStyle}>
-            <h2>Login</h2>
-            <SuperInput name={'email'} placeholder={'Enter your email'}/>
-            <SuperInput name={'password'} type={'password'} placeholder={'Enter your password'}/>
-            <div><SuperCheckbox/>
-                <span>Remember Me</span></div>
-            <SuperButton>Send</SuperButton>
-        </div>
+        <form onSubmit={submitLoginData}>
+            <div style={loginStyle}>
+                <h2>Login</h2>
+                <SuperInput name={'email'} placeholder={'Enter your email'}/>
+                <SuperInput name={'password'} type={'password'} placeholder={'Enter your password'}/>
+                <div><SuperCheckbox name={'rememberMe'} type={'checkbox'}/>
+                    <span>Remember Me</span></div>
+                <SuperButton type={'submit'}>Send</SuperButton>
+            </div>
+        </form>
     );
 }
 
