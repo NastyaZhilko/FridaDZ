b5ebb13bdf082df494a96093c47f211a462f0521
NastyaZhilko <nasten4ik2010@gmail.com>
2021-02-19 02:01:38 +0300

    commit

--- src/components/pages/packs.tsx
+++ src/components/pages/packs.tsx
@@ -7,35 +7,22 @@ import {SliderAnt} from "../common/PaginationComponent/RangeAnt/RangeAnt";
 import {Cards} from "./Cards";
 
 
+
 function Packs(){
     const options = [10, 20, 30, 40, 50]
     const packs = useSelector<any, any>((state)=>state.cards.packs)
 
     return(
         <div>
-            <h1>Packs</h1>
             <SliderAnt/>
             <SortByDate/>
             <SearchComponent/>
-            <div style={{display:'flex', justifyContent:'center'}}>
-            <div>Name</div>
-            <div>CardsCount</div>
-            <div>Update</div>
-            <button>Add</button>
-
-            <div>Cards</div>
-            </div>
             {packs.map((pack:any, index:number)=>{
                 return <div style={{display:'flex', justifyContent:'center'}}>
-                    <div key={index}>{`${pack.name} ---- ${pack.cardsCount}----${pack.updated}-----`}</div>
-                    <div>
-                        <button name={"del"} >Delete</button>
-                    </div>
-                    <div>
-                        <button name={"update"}>Update</button>
-                    </div>
-
+                    <div key={index}>{`${pack.name} ---- ${pack.cardsCount}----`}</div>
                     <Cards packId={pack._id}/>
+                <div key={index}>{`${pack.name} ---- ${pack.cardsCount}----`}</div>
+                <Cards packId={pack._id}/>
                 </div>
             })}
             <PaginationComponent  options={options}/>
@@ -44,3 +31,4 @@ function Packs(){
     )
 }
 export default Packs
+
