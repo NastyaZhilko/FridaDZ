d7553821f8086fa0ba60a4a4ada1586f891cb1d8
NastyaZhilko <nasten4ik2010@gmail.com>
2021-06-11 01:04:24 +0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,17 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import './App.css';
 import {HashRouter} from "react-router-dom";
 import Routes from "./components/routes/Routes";
 import Header from "./components/header/Header";
-import {useDispatch} from "react-redux";
 import 'rc-slider/assets/index.css';
-import {getPacksTC} from "./store/packs-reducer";
-
-
-
 
 const App = () => {
-
-
-
-
 
     return (
         <div className="App">
             <HashRouter>
-                    <Header/>
-                    <Routes/>
-
+                <Header/>
+                <Routes/>
             </HashRouter>
         </div>
     );
